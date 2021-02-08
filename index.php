22ee2c41557b1f58e30d212bfcbacf70b09f2907
Stricker97 <70325983+Stricker97@users.noreply.github.com>
2021-02-08 15:29:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
-<html lang="en">
+html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kinoverwaltung</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Hello Lucas</h1>
+    <header>
+      <h1>Welcome to CineLucas</h1>
+    </header>
+    <main>
+        <button>Sitz buchen</button>
+    </main>
 </body>
 </html>
