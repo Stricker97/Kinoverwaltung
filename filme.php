5ae0bea08e4a1c5d35b06bf36d47ceeb9c1bc965
Stricker97 <70325983+Stricker97@users.noreply.github.com>
2021-02-10 16:07:37 +0100

    Update filme.php

--- filme.php
+++ filme.php
@@ -4,8 +4,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filme</title>
     <link rel="stylesheet" href="filme.css">
+    <script src="https://kit.fontawesome.com/e742385434.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Filme</h1>
+    <header>
+        <div class="home">
+            <a href="index.php"><i class="fas fa-home"></i></a>
+        </div>
+        <h1>Filme</h1>
+    </header>
+    <main>
+        <div class="film">
+            <div class="info">
+                <p>Film Titel:</p>
+                <p>Dauer:</p>
+                <p>Startzeit:</p>
+                <p>Endzeit:</p>
+                <p>Saal:</p>
+            </div>
+        </div>
+        <div class="film">
+            <div class="info">
+                <p>Film Titel:</p>
+                <p>Dauer:</p>
+                <p>Startzeit:</p>
+                <p>Endzeit:</p>
+                <p>Saal:</p>
+            </div>
+        </div>
+        <div class="film">
+            <div class="info">
+                <p>Film Titel:</p>
+                <p>Dauer:</p>
+                <p>Startzeit:</p>
+                <p>Endzeit:</p>
+                <p>Saal:</p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
