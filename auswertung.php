5f2fd98375f2374cf5e9b8d7af640259dfd4173f
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2021-02-12 14:09:34 +0100

    Add files via upload

---
+++ auswertung.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filme</title>
+    <link rel="stylesheet" href="filme.css">
+    <script src="https://kit.fontawesome.com/e742385434.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <div class="home">
+            <a href="index.php"><i class="fas fa-home"></i></a>
+        </div>
+        <h1>Auswertung</h1>
+    </header>
+    <main>
+        <div class="film">
+            <div class="info">
+                <p>Film Titel:</p>
+                <p>Saal:</p>
+                <p>Geld: </p>
+                <p>Besetzung: </p>
+            </div>
+        </div>
+        <div class="film">
+            <div class="info">
+            <p>Film Titel:</p>
+                <p>Saal:</p>
+                <p>Geld: </p>
+                <p>Besetzung: </p>
+            </div>
+        </div>
+        <div class="film">
+            <div class="info">
+                <p>Film Titel:</p>
+                <p>Saal:</p>
+                <p>Geld: </p>
+                <p>Besetzung: </p>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
